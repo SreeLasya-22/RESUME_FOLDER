fbc8eee8cb0e37ae81c7cc545290f913e86b2a42
SreeLasya-22 <lasya.paidi7@gmail.com>
2025-12-10 22:19:22 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,27 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# myproject/urls.py
 from django.contrib import admin
 from django.urls import path
 from resume_app import views
-
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Home / Resume Form
     path('', views.resume_form, name='resume_form'),
-
-    # Preview page with optional template choice
-    path('resume/<int:resume_id>/', views.resume_preview, name='resume_preview'),
-
-    # PDF download
-    path('download_pdf/<int:resume_id>/', views.download_resume_pdf, name='download_resume_pdf'),
+    path("", include("resume_app.urls")), 
+    path('preview/<int:resume_id>/', views.resume_preview, name='resume_preview'),
 ]
